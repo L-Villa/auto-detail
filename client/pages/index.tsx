a09fcf346f9fd9c96c32d4c02a12637e7b4731f4
L-Villa <63620359+L-Villa@users.noreply.github.com>
2021-10-15 17:54:57 -0700

    Completed how section

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from "next";
 import Hero from "../layouts/home/Hero";
+import How from "../layouts/home/How";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <How />
+    </>
+  );
 };
 
 export default Home;
