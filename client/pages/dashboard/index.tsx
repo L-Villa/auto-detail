04048a34a488700a7b99c5e439d663fa59af6953
L-Villa <63620359+L-Villa@users.noreply.github.com>
2022-02-04 19:21:33 -0800

    Set up dashboard files

---
+++ client/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Dashboard from "../../layouts/dashboard/Dashboard";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+};
+
+export default Home;
