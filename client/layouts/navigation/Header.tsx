fd651b7b20f80d9295367af859e1c352476221e6
L-Villa <63620359+L-Villa@users.noreply.github.com>
2022-02-16 17:50:22 -0800

    Created basic header

--- client/layouts/navigation/Header.tsx
+++ client/layouts/navigation/Header.tsx
@@ -1,12 +1,41 @@
 import React from "react";
+import Link from "next/link";
+import { Home } from "../../public/assets/vectors/icons";
 
 const Header = () => {
   return (
     <header>
       <div className="container">
         <div className="row space-between align-center">
-          <div className="logo"></div>
-          <div className="menu"></div>
+          <Link href="/">
+            <a>
+              <div className="logo">
+                <Home />
+              </div>
+            </a>
+          </Link>
+          <ul className="menu">
+            <Link href="/dashboard/home">
+              <a>
+                <li>About</li>
+              </a>
+            </Link>
+            <Link href="/dashboard/home">
+              <a>
+                <li>Contact</li>
+              </a>
+            </Link>
+            <Link href="/dashboard/home">
+              <a>
+                <li>Log in</li>
+              </a>
+            </Link>
+            <Link href="/dashboard/home">
+              <a>
+                <li>Sign up</li>
+              </a>
+            </Link>
+          </ul>
         </div>
       </div>
     </header>
